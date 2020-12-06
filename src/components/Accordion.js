c51407e7c870175b9c9d8e5a59b68a76a92bf593
leonelros <rleonel426@gmail.com>
2020-12-05 17:06:01 -0800

    adding the structure of the app

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  //we pass items from the items arraya data in app.js and index because we want to show the the element clicked based on their index
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
-      <div key={item.title}>
-        <div className="title active">
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
-      </div>
+      </React.Fragment>
     );
   });
 
