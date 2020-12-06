6fd729cfb55b7abb5bb0bef682e3eddfb64bb641
leonelros <rleonel426@gmail.com>
2020-12-05 16:39:18 -0800

    adding the structure of the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Accordion } from "./components/Accordion";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello and welcome to Leo's React project</div>;
-}
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
 
 export default App;
