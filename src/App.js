35586925ff6687688219bc5b933d2cdb5c608385
leonelros <rleonel426@gmail.com>
2020-12-10 15:32:46 -0800

    nav bar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 import "./App.css";
 
 const items = [
@@ -38,26 +40,28 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <Accordion items={items} />
-      <br />
-      <Search />
-      <br />
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        toggle dropdown
-      </button>
-      {showDropdown ? (
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
         <Dropdown
           label="Select a color"
+          options={options}
           selected={selected}
           onSelectedChange={setSelected}
-          options={options}
         />
-      ) : null}
-      <Translate />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
